ef16860948ac97a0c3a3aafb6923c854b6e9b863
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2022-08-22 18:49:23 +0800

    metrics

--- internal/server/metaserver_grpc.go
+++ internal/server/metaserver_grpc.go
@@ -2,38 +2,80 @@ package server
 
 import (
 	"context"
+	"time"
 
+	"github.com/rfeverything/rfs/internal/metrics"
 	mpb "github.com/rfeverything/rfs/internal/proto/meta_server"
 )
 
 func (m *MetaServer) CreateFile(ctx context.Context, req *mpb.CreateFileRequest) (*mpb.CreateFileResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("CreateFile").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("CreateFile").Observe(time.Since(start).Seconds())
+	}()
 	return m.metaserver.CreateFile(ctx, req)
 }
 
 func (m *MetaServer) AppendFile(ctx context.Context, req *mpb.AppendFileRequest) (*mpb.AppendFileResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("AppendFile").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("AppendFile").Observe(time.Since(start).Seconds())
+	}()
 	return m.metaserver.AppendFile(ctx, req)
 }
 
 func (m *MetaServer) GetFile(ctx context.Context, req *mpb.GetFileRequest) (*mpb.GetFileResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("GetFile").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("GetFile").Observe(time.Since(start).Seconds())
+	}()
 	return m.metaserver.GetFile(ctx, req)
 }
 
 func (m *MetaServer) Stat(ctx context.Context, req *mpb.StatRequest) (*mpb.StatResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("Stat").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("Stat").Observe(time.Since(start).Seconds())
+	}()
 	return m.metaserver.Stat(ctx, req)
 }
 
 func (m *MetaServer) Remove(ctx context.Context, req *mpb.RemoveRequest) (*mpb.RemoveResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("Remove").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("Remove").Observe(time.Since(start).Seconds())
+	}()
 	return m.metaserver.Remove(ctx, req)
 }
 
 func (m *MetaServer) Mkdir(ctx context.Context, req *mpb.MkdirRequest) (*mpb.MkdirResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("Mkdir").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("Mkdir").Observe(time.Since(start).Seconds())
+	}()
 	return m.metaserver.Mkdir(ctx, req)
 }
 
 func (m *MetaServer) List(ctx context.Context, req *mpb.ListRequest) (*mpb.ListResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("List").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("List").Observe(time.Since(start).Seconds())
+	}()
 	return m.metaserver.List(ctx, req)
 }
 
 func (m *MetaServer) Move(ctx context.Context, req *mpb.MoveRequest) (*mpb.MoveResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("Move").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("Move").Observe(time.Since(start).Seconds())
+	}()
 	return m.metaserver.Move(ctx, req)
 }
--- internal/server/volumeserver_grpc.go
+++ internal/server/volumeserver_grpc.go
@@ -2,20 +2,43 @@ package server
 
 import (
 	"context"
+	"time"
+
+	"github.com/rfeverything/rfs/internal/metrics"
 
 	vpb "github.com/rfeverything/rfs/internal/proto/volume_server"
 )
 
 func (vs *VolumeServer) PutChunk(ctx context.Context, req *vpb.PutChunkRequest) (*vpb.PutChunkResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("PutChunk").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("PutChunk").Observe(time.Since(start).Seconds())
+	}()
 	return vs.vs.PutChunk(ctx, req)
 }
 func (vs *VolumeServer) GetChunk(ctx context.Context, req *vpb.GetChunkRequest) (*vpb.GetChunkResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("GetChunk").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("GetChunk").Observe(time.Since(start).Seconds())
+	}()
 	return vs.vs.GetChunk(ctx, req)
 }
 func (vs *VolumeServer) DeleteChunk(ctx context.Context, req *vpb.DeleteChunkRequest) (*vpb.DeleteChunkResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("DeleteChunk").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("DeleteChunk").Observe(time.Since(start).Seconds())
+	}()
 	return vs.vs.DeleteChunk(ctx, req)
 }
 func (vs *VolumeServer) VolumeStatus(ctx context.Context, req *vpb.VolumeStatusRequest) (*vpb.VolumeStatusResponse, error) {
+	metrics.VolumeServerRequestCounter.WithLabelValues("VolumeStatus").Inc()
+	start := time.Now()
+	defer func() {
+		metrics.VolumeServerRequestDuration.WithLabelValues("VolumeStatus").Observe(time.Since(start).Seconds())
+	}()
 	return vs.vs.VolumeStatus(ctx, req)
 }
 func (vs *VolumeServer) Ping(ctx context.Context, req *vpb.PingRequest) (*vpb.PingResponse, error) {
