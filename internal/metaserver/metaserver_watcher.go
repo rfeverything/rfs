3c430c3980d734de63e8b5a28b0451da38dd87e8
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2022-08-22 23:01:04 +0800

    Update metaserver_watcher.go

--- internal/metaserver/metaserver_watcher.go
+++ internal/metaserver/metaserver_watcher.go
@@ -6,6 +6,15 @@ import (
 )
 
 func (ms *MetaServer) watchVolumeState() {
+	sts, err := ms.Store.GetVolumesStatus()
+	if err != nil {
+		logger.Global().Error("failed to get volumes status", zap.Error(err))
+		return
+	}
+	for _, st := range sts {
+		ms.VolumeClients[st.VolumeId] = NewVolumeClient(st.Address)
+	}
+
 	for event := range ms.Store.GetVolumesStatusChan() {
 		logger.Global().Info("watchVolumeState", zap.Any("event", event))
 		if event.VolumeId == "" {
